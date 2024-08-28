2573d1f971fbc711c4563bbe352d579b8871ecd0
filipe <filipe.sa1@ibm.com>
2024-08-28 02:28:06 +0100

    xxx

--- src/datasources/postgres-sql.datasource.ts
+++ src/datasources/postgres-sql.datasource.ts
@@ -1,5 +1,5 @@
-import {inject,lifeCycleObserver,LifeCycleObserver} from '@loopback/core';
-import {juggler} from '@loopback/repository';
+import {inject,lifeCycleObserver,LifeCycleObserver} from "@loopback/core";
+import {juggler} from "@loopback/repository";
 /** DEV
  *
  *
@@ -28,54 +28,70 @@ const config ={
 }
 */
 //postgresql://pihh:hXHtyGLyqKyCz0jczvlfdZhZvkTSBGnw@dpg-cr72mgd6l47c7382pr40-a:5432/cdb_4om1
-const config = {
+const ___config = {
+  name: "cc-db",
+  connector: "postgresql",
 
+  host: "dpg-cr72mgd6l47c7382pr40-a.frankfurt-postgres.render.com",
+
+  url: "",
+
+  port: 5432,
+  // user: 'pihh.rocks@gmail.com',
+  user: "pihh",
+  password: "hXHtyGLyqKyCz0jczvlfdZhZvkTSBGnw",
+  database: "cdb_4om1",
+
+  // ssl: {
+  //   rejectUnauthorized: false, // This allows self-signed certificates; set it to true for stricter security
+  // },
+};
+
+const config = {
+  name: "cc-db",
+  connector: "postgresql",
+
+  // host: "dpg-cr72mgd6l47c7382pr40-a.frankfurt-postgres.render.com",
+  // host: "dpg-cr72mgd6l47c7382pr40-a",
+  host: "dpg-cr72mgd6l47c7382pr40-a.onrender.com",
+
+  url: "",
+
+  port: 5432,
+   user: 'pihh',
+  //user: "pihh",
+  password: "hXHtyGLyqKyCz0jczvlfdZhZvkTSBGnw",
+  database: "cdb_4om1",
+  ssl:true
+
+  // dpg-cr72mgd6l47c7382pr40-a.frankfurt-postgres.render.com
+
+}
+
+// pg admin pass f5bd4decf0539d7810f389cc8763e1a2
+// dpg-cr72mgd6l47c7382pr40-a
+const _config = {
   name: 'cc-db',
   connector: 'postgresql',
-  // url: '/pihh:hXHtyGLyqKyCz0jczvlfdZhZvkTSBGnw@dpg-cr72mgd6l47c7382pr40-a.frankfurt-postgres.render.com/cdb_4om1',
-  // host: 'pg-cr72mgd6l47c7382pr40-a:5432',
-  host: 'dpg-cr72mgd6l47c7382pr40-a.frankfurt-postgres.render.com',
-  // dpg-cr72mgd6l47c7382pr40-a.frankfurt-postgres.render.com
-  //host: '216.24.57.252',
-  // port: 5432,
-  // user: 'pihh',
-  // password: 'hXHtyGLyqKyCz0jczvlfdZhZvkTSBGnw',
-  // database: 'cdb_4om1'
-
-
-  //   name: 'pgdb',
-    // connector: 'postgresql',
-    url: '',
-    // url: 'postgresql://pihh:hXHtyGLyqKyCz0jczvlfdZhZvkTSBGnw@dpg-cr72mgd6l47c7382pr40-a.frankfurt-postgres.render.com/cdb_4om1',
-    // host: 'dpg-cr72mgd6l47c7382pr40-a.frankfurt-postgres.render.com',
-    port: 5432,
-    // user: 'pihh.rocks@gmail.com',
-    user: 'pihh',
-    password: 'hXHtyGLyqKyCz0jczvlfdZhZvkTSBGnw',
-    database: 'cdb_4om1',  ssl: {
-      rejectUnauthorized: false, // This allows self-signed certificates; set it to true for stricter security
-    },
-    //database: 'postgres'
-  // };
-
+  url: 'postgresql://pihh:hXHtyGLyqKyCz0jczvlfdZhZvkTSBGnw@dpg-cr72mgd6l47c7382pr40-a.frankfurt-postgres.render.com:5432/cdb_4om1?ssl=true&sslmode=require',
 };
+
 
 // Observe application's life cycle to disconnect the datasource when
 // application is stopped. This allows the application to be shut down
 // gracefully. The `stop()` method is inherited from `juggler.DataSource`.
 // Learn more at https://loopback.io/doc/en/lb4/Life-cycle.html
-@lifeCycleObserver('datasource')
+@lifeCycleObserver("datasource")
 export class PostgresSqlDataSource extends juggler.DataSource
   implements LifeCycleObserver {
-  static dataSourceName = 'PostgresSql';
+  static dataSourceName = "PostgresSql";
   static readonly defaultConfig = config;
 
   constructor(
-    @inject('datasources.config.PostgresSql', {optional: true})
-    dsConfig: object = config,
+    @inject("datasources.config.PostgresSql", { optional: true })
+    dsConfig: object = config
   ) {
-
-    console.log(dsConfig)
-        super(dsConfig);
+    console.log(dsConfig);
+    super(dsConfig);
   }
 }
