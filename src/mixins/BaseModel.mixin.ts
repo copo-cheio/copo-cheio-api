29f184751634c12377a7ff120b11aca2c6f9864e
filipe <filipe.sa1@ibm.com>
2024-08-28 02:33:01 +0100

    no mixin

--- src/mixins/BaseModel.mixin.ts
+++ src/mixins/BaseModel.mixin.ts
@@ -1,50 +1,50 @@
-import {MixinTarget} from '@loopback/core';
-import {Entity,model,property} from '@loopback/repository';
+// import {MixinTarget} from '@loopback/core';
+// import {Entity,model,property} from '@loopback/repository';
 
-export function TimestampMixin<T extends MixinTarget<Entity>>(Base: T) {
-  @model()
-  class TimestampedEntity extends Base {
-    @property({
-      type: 'string',
-      id: true,
-      generated: true,
-      useDefaultIdType: false,
-      postgresql: {
-        dataType: 'uuid',
-        extension: 'pgcrypto',
-        defaultFn: 'gen_random_uuid()', //<---- only this line is different
-      },
+// export function TimestampMixin<T extends MixinTarget<Entity>>(Base: T) {
+//   @model()
+//   class TimestampedEntity extends Base {
+//     @property({
+//       type: 'string',
+//       id: true,
+//       generated: true,
+//       useDefaultIdType: false,
+//       postgresql: {
+//         dataType: 'uuid',
+//         extension: 'pgcrypto',
+//         defaultFn: 'gen_random_uuid()', //<---- only this line is different
+//       },
 
-    })
-    id?: string;
+//     })
+//     id?: string;
 
-    @property({
-      type: 'string',
-      required: true,
-      jsonSchema: {
-        format: 'date-time',
-      },
-      defaultFn: 'now',
-    })
-    createdAt: string;
+//     @property({
+//       type: 'string',
+//       required: true,
+//       jsonSchema: {
+//         format: 'date-time',
+//       },
+//       defaultFn: 'now',
+//     })
+//     createdAt: string;
 
-    @property({
-      type: 'string',
-      required: true,
-      jsonSchema: {
-        format: 'date-time',
-      },
-    })
-    updatedAt: string;
+//     @property({
+//       type: 'string',
+//       required: true,
+//       jsonSchema: {
+//         format: 'date-time',
+//       },
+//     })
+//     updatedAt: string;
 
-    constructor(data?: Partial<TimestampedEntity>) {
-      super(data);
-      const now = new Date().toISOString();
-      if (!this.createdAt) {
-        this.createdAt = now;
-      }
-      this.updatedAt = now;
-    }
-  }
-  return TimestampedEntity;
-}
+//     constructor(data?: Partial<TimestampedEntity>) {
+//       super(data);
+//       const now = new Date().toISOString();
+//       if (!this.createdAt) {
+//         this.createdAt = now;
+//       }
+//       this.updatedAt = now;
+//     }
+//   }
+//   return TimestampedEntity;
+// }
